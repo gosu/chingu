6117efc6f59d1feee26058e650ff446f9ec0f739
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-01 21:07:44 -0300

    Use `describe Class` instead
    and remove use of "should" in test descriptors

--- spec/chingu/basic_game_object_spec.rb
+++ spec/chingu/basic_game_object_spec.rb
@@ -2,11 +2,9 @@
 
 require 'spec_helper'
 
-class MyBasicGameObject < Chingu::BasicGameObject
-end
+class MyBasicGameObject < Chingu::BasicGameObject; end
 
-class MyBasicGameObject2 < Chingu::BasicGameObject
-end
+class MyBasicGameObject2 < Chingu::BasicGameObject; end
 
 class MyBasicGameObjectWithSetup < Chingu::BasicGameObject
   def setup
@@ -14,115 +12,117 @@ class MyBasicGameObjectWithSetup < Chingu::BasicGameObject
   end
 end
 
-module Chingu
-  describe BasicGameObject do
-    before :each do
-      @game = Chingu::Window.new
+describe Chingu::BasicGameObject do
+  before do
+    @game = Chingu::Window.new
+  end
+
+  after do
+    @game.close
+  end
+
+  it { is_expected.to respond_to(:options) }
+  it { is_expected.to respond_to(:paused) }
+  it { is_expected.to respond_to(:setup_trait) }
+  it { is_expected.to respond_to(:setup) }
+  it { is_expected.to respond_to(:update_trait) }
+  it { is_expected.to respond_to(:draw_trait) }
+  it { is_expected.to respond_to(:filename) }
+
+  context 'With a class inherited from BasicGameObject using #create' do
+    it 'is automatically stored in $window.game_objects' do
+      MyBasicGameObject.instances = []
+      3.times { MyBasicGameObject.create }
+
+      expect($window.game_objects.size).to eq(3)
     end
 
-    after :each do
-      @game.close
+    it 'has $window as parent' do
+      go = MyBasicGameObject.create
+
+      expect(go.parent).to eq($window)
     end
 
-    it { should respond_to(:options) }
-    it { should respond_to(:paused) }
-    it { should respond_to(:setup_trait) }
-    it { should respond_to(:setup) }
-    it { should respond_to(:update_trait) }
-    it { should respond_to(:draw_trait) }
-    it { should respond_to(:filename) }
+    it 'keeps track of its instances in #all' do
+      MyBasicGameObject.instances = []
+      3.times { MyBasicGameObject.create }
 
-    context 'A class inherited from BasicGameObject using classmethod create' do
-      it 'should be automatically stored in $window.game_objects' do
-        MyBasicGameObject.instances = []
-        3.times { MyBasicGameObject.create }
-        expect($window.game_objects.size).to eq(3)
-      end
-
-      it 'should have $window as parent' do
-        go = MyBasicGameObject.create
-        expect(go.parent).to eq($window)
-      end
-
-      it 'should keep track of its instances in class#all' do
-        MyBasicGameObject.instances = []
-        3.times { MyBasicGameObject.create }
-        #
-        # Can/should we remove the dependency on #update here before the created objects gets saved?
-        # We mostly protect against adding to the object array while iterating over it
-        #
-        expect(MyBasicGameObject.all.size).to eq(3)
-        expect(MyBasicGameObject.size).to eq(3)
-      end
-
-      it 'should be removed from game_objects list when destroy() is called' do
-        MyBasicGameObject.instances = []
-        go = MyBasicGameObject.create
-        expect($window.game_objects.size).to eq(1)
-
-        go.destroy
-        expect($window.game_objects.size).to eq(0)
-      end
-
-      it 'should have all internal list cleared with classmethod destroy_all()' do
-        MyBasicGameObject.instances = []
-        3.times { MyBasicGameObject.create }
-        MyBasicGameObject.destroy_all
-
-        expect(MyBasicGameObject.size).to eq(0)
-      end
-
-      it 'should have all instances removed from parent-list with classmethod destroy_all()' do
-        MyBasicGameObject.instances = []
-        3.times { MyBasicGameObject.create }
-        MyBasicGameObject.destroy_all
-
-        expect($window.game_objects.size).to eq(0)
-      end
+      # Can/should we remove the dependency on #update here before the created
+      # objects gets saved? We mostly protect against adding to the object
+      # array while iterating over it
+      expect(MyBasicGameObject.all.size).to eq(3)
+      expect(MyBasicGameObject.size).to eq(3)
     end
 
-    context 'A class inherited from BasicGameObject' do
-      it 'should return empty array on classmethod all() if no objects have been created' do
-        # Only place MyBasicGameObject2 is used
-        expect(MyBasicGameObject2.all).to eq([]) # Surely there's a better way
-      end
+    it 'is removed from game_objects list when #destroy is called' do
+      MyBasicGameObject.instances = []
+      go = MyBasicGameObject.create
+      expect($window.game_objects.size).to eq(1)
 
-      it 'should take hash-argument, parse it and save in options' do
-        MyBasicGameObject.instances = []
-        game_object = MyBasicGameObject.new(paused: false, foo: :bar)
-        expect(game_object.paused?).to be(false)
-        expect(game_object.options).to eq({ paused: false, foo: :bar })
-      end
-
-      it 'should call setup() at the end of initialization' do
-        game_object = MyBasicGameObjectWithSetup.new(paused: false)
-        expect(game_object.paused?).to be(true)
-      end
-
-      it 'should be unpaused by default' do
-        expect(subject.paused?).to be(false)
-      end
-
-      it 'should change paused status with pause()/unpause()' do
-        subject.pause
-        expect(subject.paused?).to be(true)
-        subject.unpause
-        expect(subject.paused?).to be(false)
-      end
-
-      it 'should give a well named string with filename()' do
-        expect(MyBasicGameObject.new.filename).to eq('my_basic_game_object')
-      end
+      go.destroy
+      expect($window.game_objects.size).to eq(0)
     end
 
-    context 'when created with defaults in Chingu::Window' do
-      it 'should belong to main window it not created inside a game state' do
-        expect(subject.parent).to eq(@game)
-      end
+    it 'hass all internal list cleared with #destroy_all()' do
+      MyBasicGameObject.instances = []
+      3.times { MyBasicGameObject.create }
+      MyBasicGameObject.destroy_all
+
+      expect(MyBasicGameObject.size).to eq(0)
     end
 
-    context 'when created in Chingu::GameState' do
-      # TODO
+    it 'has all instances removed from parent-list with #destroy_all()' do
+      MyBasicGameObject.instances = []
+      3.times { MyBasicGameObject.create }
+      MyBasicGameObject.destroy_all
+
+      expect($window.game_objects.size).to eq(0)
     end
+  end
+
+  context 'With a class inherited from BasicGameObject' do
+    it 'returns empty array on #all if no objects have been created' do
+      # Only place MyBasicGameObject2 is used
+      expect(MyBasicGameObject2.all).to eq([])
+    end
+
+    it 'takes extra keyword arguments, parses and saves them in options' do
+      MyBasicGameObject.instances = []
+      game_object = MyBasicGameObject.new(paused: false, foo: :bar)
+
+      expect(game_object.paused?).to be_falsy
+      expect(game_object.options).to eq({ paused: false, foo: :bar })
+    end
+
+    it 'calls #setup at the end of initialization' do
+      game_object = MyBasicGameObjectWithSetup.new(paused: false)
+
+      expect(game_object.paused?).to be_truthy
+    end
+
+    it 'is unpaused by default' do
+      expect(subject.paused?).to be_falsy
+    end
+
+    it 'changes paused status with #pause/#unpause' do
+      subject.pause
+      expect(subject.paused?).to be_truthy
+      subject.unpause
+      expect(subject.paused?).to be_falsy
+    end
+
+    it 'gives a correctly named string with #filename' do
+      expect(MyBasicGameObject.new.filename).to eq('my_basic_game_object')
+    end
+  end
+
+  context 'When created with defaults in Chingu::Window' do
+    it 'belongs to main window if not created inside a game state' do
+      expect(subject.parent).to eq(@game)
+    end
+  end
+
+  context 'When created in Chingu::GameState' do
+    # TODO
   end
 end
