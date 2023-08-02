26595416b01dab7ceca50d71bd12996a1cae0ab3
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-01 21:10:39 -0300

    Use `describe Class` instead
    and remove use of "should" in test descriptors

--- spec/chingu/helpers/input_dispatcher_spec.rb
+++ spec/chingu/helpers/input_dispatcher_spec.rb
@@ -3,87 +3,106 @@
 require 'spec_helper'
 
 describe Chingu::Helpers::InputDispatcher do
-  before :each do
+  before do
     @subject = Object.new.extend described_class
     @client = Object.new
   end
 
   it 'should respond to methods' do
-    @subject.should respond_to :input_clients
-    @subject.should respond_to :add_input_client
-    @subject.should respond_to :remove_input_client
+    expect(@subject).to respond_to(:input_clients)
+    expect(@subject).to respond_to(:add_input_client)
+    expect(@subject).to respond_to(:remove_input_client)
   end
 
   { 'button_down' => :a, 'button_up' => :released_a }.each_pair do |event, key|
     describe "#dispatch_#{event}" do
-      it 'should dispatch key event if key is handled' do
-        @client.should_receive(:handler).with(no_args)
-        allow(@client).to receive(:input).with(no_args) { { key => [@client.method(:handler)] } }
+      it 'dispatchs key event if key is handled' do
+        expect(@client).to receive(:handler).with(no_args)
+
+        allow(@client).to receive(:input).with(no_args) {
+           { key => [@client.method(:handler)] }
+         }
+
         @subject.send("dispatch_#{event}", Gosu::KbA, @client)
       end
 
-      it 'should not dispatch key event if key is not handled' do
+      it 'does not dispatch key event if key is not handled' do
         allow(@client).to receive(:input).with(no_args) { {} }
+
         @subject.send("dispatch_#{event}", Gosu::KbA, @client)
       end
     end
   end
 
   describe '#dispatch_input_for' do
-    before :each do
+    before do
       $window = double(Chingu::Window)
       allow($window).to receive(:button_down?) { false }
     end
 
-    after :each do
+    after do
       $window = nil
     end
 
-    it 'should dispatch if a key is being held' do
-      @client.should_receive(:handler).with(no_args)
+    it 'dispatchs if a key is being held' do
+      expect(@client).to receive(:handler).with(no_args)
+
       allow($window).to receive(:button_down?).with(Gosu::KbA) { true }
+
       allow(@client).to receive(:input).with(no_args) { { holding_a: [@client.method(:handler)] } }
       @subject.dispatch_input_for(@client)
     end
 
-    it 'should do nothing if a key is not held' do
-      allow(@client).to receive(:input).with(no_args) { { holding_a: [-> { raise "Shouldn't handle input!" }] } }
+    it 'do nothing if a key is not held' do
+      allow(@client).to receive(:input).with(no_args) {
+        { holding_a: [-> { raise "Shouldn't handle input!" }] }
+
+      }
       @subject.dispatch_input_for(@client)
     end
   end
 
   describe '#dispatch_actions' do
-    it 'should call a method' do
-      @client.should_receive(:handler).with(no_args)
+    it 'calls a method' do
+      expect(@client).to receive(:handler).with(no_args)
+
       @subject.send(:dispatch_actions, [@client.method(:handler)])
     end
 
-    it 'should call a proc' do
-      @client.should_receive(:handler)
+    it 'calls a proc' do
+      expect(@client).to receive(:handler)
+
       @subject.send(:dispatch_actions, [-> { @client.handler }])
     end
 
-    it 'should push a game-state instance' do
+    it 'will push a game-state instance' do
       state = Chingu::GameState.new
-      @subject.should_receive(:push_game_state).with(state)
+
+      expect(@subject).to receive(:push_game_state).with(state)
+
       @subject.send(:dispatch_actions, [state])
     end
 
-    it 'should push a game-state class' do
-      @subject.should_receive(:push_game_state).with(Chingu::GameState)
+    it 'will push a game-state class' do
+      expect(@subject).to receive(:push_game_state).with(Chingu::GameState)
+
       @subject.send(:dispatch_actions, [Chingu::GameState])
     end
 
-    it 'should call multiple actions if more have been set' do
+    it 'calls multiple actions if more have been set' do
       other = Object.new
-      other.should_receive(:handler).with(no_args)
-      @client.should_receive(:handler).with(no_args)
+      expect(other).to receive(:handler).with(no_args)
+
+      expect(@client).to receive(:handler).with(no_args)
+
       @subject.send(:dispatch_actions, [@client.method(:handler), other.method(:handler)])
     end
 
-    # Note, doesn't check if a passed class is incorrect. Life is too short.
-    it 'should raise an error with unexpected data' do
-      -> { @subject.send(:dispatch_actions, [12]) }.should raise_error ArgumentError
+    # NOTE: Doesn't check if a passed class is incorrect. Life is too short.
+    it 'raises an error with unexpected data' do
+      expect {
+        @subject.send(:dispatch_actions, [12])
+      }.to raise_error(ArgumentError)
     end
   end
 end
