d757054cd606e6a9053727fac947c5c9a1ab7cd3
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-01 21:11:16 -0300

    Use `describe Class` instead
    and remove use of "should" in test descriptors

--- spec/chingu/input_spec.rb
+++ spec/chingu/input_spec.rb
@@ -3,32 +3,35 @@
 require 'spec_helper'
 
 describe Chingu::Input do
-  it 'should map all defined Gosu input constants to Chingu symbols' do
+  it 'maps all defined Gosu input constants to Chingu symbols' do
     # Simpler if all the inputs are in a big hash.
     input_names = Gosu.constants.select { |constant| constant =~ /^(?:Kb|Ms|Gp)/ }
-    input_names.delete_if { |name| name.to_s =~ /Range(?:Start|End)|Num$/ } # Special entries.
+    input_names.delete_if do |name|
+      name.to_s =~ /Range(?:Start|End)|Num$/ # Special entries.
+    end
+
     gosu_inputs = input_names.each_with_object({}) do |name, hash|
-      hash[name] = Gosu.const_get name
+      hash[name] = Gosu.const_get(name)
     end
 
     gosu_inputs.each_value do |code|
       next if code == 0 # todo, check into this, spooner? ;)
 
       symbols = described_class::CONSTANT_TO_SYMBOL[code]
-      next if symbols.nil? # FIXME: This will become nil with a specific key for
 
-      # some reason. I'd reckon since we're quite behind
-      # gosu we're missing some keys, I'll need to sit down
-      # and check it out.
+      next if symbols.nil? # FIXME: This will become nil with a specific key for
+                           # some reason. I'd reckon since we're quite behind
+                           # gosu we're missing some keys, I'll need to sit down
+                           # and check it out.
 
       expect(symbols).not_to be_empty
       symbols.each { |s| expect(s).to be_kind_of(Symbol) }
     end
   end
 
-  it 'should map all Chingu symbols to Gosu input codes' do
+  it 'maps all Chingu symbols to Gosu input codes' do
     described_class::CONSTANT_TO_SYMBOL.values.flatten.uniq.each do |symbol|
-      described_class::SYMBOL_TO_CONSTANT[symbol].should be_kind_of Integer
+      expect(described_class::SYMBOL_TO_CONSTANT[symbol]).to be_kind_of(Integer)
     end
   end
 end
