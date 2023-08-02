244ff1392c9fa22f136d9937e2ac1b7d36eb3f64
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-02 14:10:47 -0300

    Update to new rspec syntax

--- spec/chingu/inflector_spec.rb
+++ spec/chingu/inflector_spec.rb
@@ -2,18 +2,16 @@
 
 require 'spec_helper'
 
-module Chingu
-  describe Inflector do
-    describe '.camelize' do
-      it 'camelizes strings' do
-        subject.camelize('automatic_assets').should eql('AutomaticAssets')
-      end
+describe Chingu::Inflector do
+  describe '#camelize' do
+    it 'camelizes strings' do
+      expect(subject.camelize('automatic_assets')).to eql('AutomaticAssets')
     end
+  end
 
-    describe '.underscore' do
-      it 'converts class-like strings to underscore' do
-        subject.underscore('FireBall').should eql('fire_ball')
-      end
+  describe '#underscore' do
+    it 'converts class-like strings to underscore' do
+      expect(subject.underscore('FireBall')).to eql('fire_ball')
     end
   end
 end
