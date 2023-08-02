9b7db0df56e264f68f35d63493b3ae64fe65734c
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-01 21:08:45 -0300

    Use `describe Class` instead
    and remove use of "should" in test descriptors

--- spec/chingu/console_spec.rb
+++ spec/chingu/console_spec.rb
@@ -2,48 +2,49 @@
 
 require 'spec_helper'
 
-module Chingu
-  describe 'Console' do
-    before :each do
-      @console = Chingu::Console.new
+describe Chingu::Console do
+  before do
+    @console = Chingu::Console.new
+  end
+
+  after do
+    $window = nil
+  end
+
+  it { is_expected.to respond_to(:start) }
+  it { is_expected.to respond_to(:fps) }
+  it { is_expected.to respond_to(:update) }
+  it { is_expected.to respond_to(:root) }
+  it { is_expected.to respond_to(:game_state_manager) }
+  it { is_expected.to respond_to(:root) }
+  it { is_expected.to respond_to(:milliseconds_since_last_tick) }
+
+  context 'When initialized' do
+    it 'returns itself as current scope' do
+      expect(@console.current_scope).to eq(@console)
     end
 
-    after :each do
-      $window = nil
+    it 'has 0 game objects' do
+      expect(@console.game_objects.size).to eq(0)
+    end
+  end
+
+  context 'Each game iteration' do
+    it '#update() should call update() on all unpaused game objects' do
+      expect(Chingu::GameObject.create).to receive(:update)
+      expect(Chingu::GameObject.create(paused: true)).not_to receive(:update)
+
+      @console.update
     end
 
-    it { expect(@console).to respond_to(:start) }
-    it { expect(@console).to respond_to(:fps) }
-    it { expect(@console).to respond_to(:update) }
-    it { expect(@console).to respond_to(:root) }
-    it { expect(@console).to respond_to(:game_state_manager) }
-    it { expect(@console).to respond_to(:root) }
-    it { expect(@console).to respond_to(:milliseconds_since_last_tick) }
+    it 'increments $window.ticks' do
+      expect(@console.ticks).to eq(0)
 
-    context 'a new Chingu::Console' do
-      it 'should return itself as current scope' do
-        expect(@console.current_scope).to eq(@console)
-      end
+      @console.update
+      expect(@console.ticks).to eq(1)
 
-      it 'should have 0 game objects' do
-        expect(@console.game_objects.size).to eq(0)
-      end
-    end
-
-    context 'each game iteration' do
-      it '@console.update() should call update() on all unpaused game objects' do
-        expect(GameObject.create).to receive(:update)
-        expect(GameObject.create(paused: true)).not_to receive(:update)
-        @console.update
-      end
-
-      it 'should increment $window.ticks' do
-        expect(@console.ticks).to eq(0)
-        @console.update
-        expect(@console.ticks).to eq(1)
-        @console.update
-        expect(@console.ticks).to eq(2)
-      end
+      @console.update
+      expect(@console.ticks).to eq(2)
     end
   end
 end
