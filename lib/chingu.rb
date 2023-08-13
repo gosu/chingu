53ba38961359a419861059f4faa4f20bc29cc883
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-13 14:54:34 -0300

    Use `require_relative` instead of joining to get the path

--- lib/chingu.rb
+++ lib/chingu.rb
@@ -27,7 +27,8 @@ require 'yaml'
 require 'timeout'
 require 'socket'
 
-require File.join(CHINGU_ROOT,"chingu","require_all") # Thanks to http://github.com/tarcieri/require_all !
+require_relative 'chingu/require_all' # Thanks to
+                                      # http://github.com/tarcieri/require_all !
 
 # Seems like we need to include chingu/helpers first for BasicGameObject
 # and GameObject to get the correct class_inheritable_accssor
