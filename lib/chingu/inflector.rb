07ffc5cd310d68f763cb16203a46d463177b563c
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-23 12:56:41 -0300

    Break down really long lines into somethig more manageable, and use double quotes for string literals

--- lib/chingu/inflector.rb
+++ lib/chingu/inflector.rb
@@ -22,15 +22,18 @@
 
 module Chingu
   module Inflector
-
-		#
-		# "automatic_assets" -> "AutomaticAssets"
-		#
+    #
+    # "automatic_assets" -> "AutomaticAssets"
+    #
     def Inflector.camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
       if first_letter_in_uppercase
-        lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+        lower_case_and_underscored_word.to_s
+                                       .gsub(/\/(.?)/) { "::#{$1.upcase}" }
+                                       .gsub(/(?:^|_)(.)/) { $1.upcase }
       else
-        lower_case_and_underscored_word.first.downcase + camelize(lower_case_and_underscored_word)[1..-1]
+        lower_case_and_underscored_word.first.downcase + camelize(
+                                                           lower_case_and_underscored_word
+                                                         )[1..-1]
       end
     end
 
@@ -38,18 +41,19 @@ module Chingu
     # "Chingu::GameObject" -> "GameObject"
     #
     def Inflector.demodulize(class_name_in_module)
-      class_name_in_module.to_s.gsub(/^.*::/, '')
+      class_name_in_module.to_s.gsub(/^.*::/, "")
     end
 
-		#
-		# "FireBall" -> "fire_ball"
-		#
-		def Inflector.underscore(camel_cased_word)
-			camel_cased_word.to_s.gsub(/::/, '/').gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-			gsub(/([a-z\d])([A-Z])/,'\1_\2').
-			tr("-", "_").
-			downcase
-		end
-
+    #
+    # "FireBall" -> "fire_ball"
+    #
+    def Inflector.underscore(camel_cased_word)
+      camel_cased_word.to_s
+                      .gsub(/::/, "/")
+                      .gsub(/([A-Z]+)([A-Z][a-z])/, "\1_\2")
+                      .gsub(/([a-z\d])([A-Z])/, "\1_\2")
+                      .tr("-", "_")
+                      .downcase
+    end
   end
 end
